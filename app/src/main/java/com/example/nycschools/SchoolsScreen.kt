b16d09ecc6d5f48da274993a4806bad7485721d8
Luke Brown <Luketheduke48@gmail.com>
2023-07-12 16:00:04 -0400

    Accordion Menu

--- app/src/main/java/com/example/nycschools/SchoolsScreen.kt
+++ app/src/main/java/com/example/nycschools/SchoolsScreen.kt
@@ -1,15 +1,22 @@
 package com.example.nycschools
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -19,12 +26,13 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.nycschools.model.Schools
 import com.example.nycschools.viewmodel.SchoolsViewModel
-import org.jetbrains.annotations.TestOnly
 
 
 @Composable
@@ -52,22 +60,73 @@ fun SchoolItem(school: Schools) {
         modifier = Modifier
             .fillMaxWidth()
             .padding(horizontal = 16.dp, vertical = 8.dp)
-            .clickable { /* Handle click event */ },
-        shape = MaterialTheme.shapes.medium,
     ) {
-        Column(Modifier.padding(16.dp)) {
-            Text(
-                text = school.school_name,
-                style = MaterialTheme.typography.headlineLarge,
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(
-                text = school.dbn,
-                style = MaterialTheme.typography.titleMedium
-            )
+        Accordion(title = school.school_name) {
+            Column(Modifier.padding(16.dp)) {
+
+                val satScores = school.satScores
+                if (satScores != null) {
+                    Text(
+                        text = "SAT Test Takers: ${satScores.num_of_sat_test_takers}",
+                        style = MaterialTheme.typography.titleMedium,
+                        modifier = Modifier.padding(top = 8.dp)
+                    )
+                    Text(
+                        text = "SAT Critical Reading Avg Score: ${satScores.sat_critical_reading_avg_score}",
+                        style = MaterialTheme.typography.titleMedium,
+                        modifier = Modifier.padding(top = 8.dp)
+                    )
+                    Text(
+                        text = "SAT Math Avg Score: ${satScores.sat_math_avg_score}",
+                        style = MaterialTheme.typography.titleMedium,
+                        modifier = Modifier.padding(top = 8.dp)
+                    )
+                    Text(
+                        text = "SAT Writing Avg Score: ${satScores.sat_writing_avg_score}",
+                        style = MaterialTheme.typography.titleMedium,
+                        modifier = Modifier.padding(top = 8.dp)
+                    )
+                }
+            }
         }
     }
 }
+
+@Composable
+fun Accordion(
+    title: String,
+    content: @Composable () -> Unit
+) {
+    var expanded by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { expanded = !expanded }
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Icon(
+                imageVector = if (expanded) Icons.Filled.KeyboardArrowDown else Icons.Filled.KeyboardArrowRight,
+                contentDescription = null,
+                modifier = Modifier.size(24.dp)
+            )
+            Text(
+                text = title,
+                style = MaterialTheme.typography.titleMedium,
+                modifier = Modifier.padding(start = 8.dp)
+            )
+        }
+
+        AnimatedVisibility(visible = expanded) {
+            content()
+        }
+    }
+}
+
+
 
 @Preview()
 @Composable
